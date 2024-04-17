5b169a24faeeefdbdec62407e62f9534b8f9da2f
Duzy <167315349+DuzyDev@users.noreply.github.com>
2024-04-17 13:35:11 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,21 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'FPS Menu V2'
+
+author 'SP Leaks'
+
+version '2.0'
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+
+
+client_script '@GCNAC/src/c_00.lua'
+
+client_script '@garagens_orgs/src/c_00.lua'
